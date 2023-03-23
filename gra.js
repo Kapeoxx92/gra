7dbc136e49a0bef1a299b9de8767083a08a56de1
Kapeoxx92 <kacperwojtczuk92@gmail.com>
2023-03-23 18:44:58 +0100

    dodanie intro i muzyki

--- gra.js
+++ gra.js
@@ -56,3 +56,43 @@ function gdzieJestem(){
  div2.innerHTML = "Wsiądź"
  prawydol.appendChild(div2);
 }
+function intro(){
+    const lewy = document.querySelector("#lewy");
+    const prawy = document.querySelector("#prawy");
+    var music = document.getElementById("background-music");
+
+// Play music
+music.play();
+music.volume = 0;
+    lewy.style.width = "96vw";
+    prawy.style.width = "0vw";
+    lewy.style.backgroundImage = "url('logo.png')"
+    lewy.innerHTML = "";
+    lewy.style.backgroundSize = "96vw";
+    setTimeout(function() {
+        lewy.style.width = "47vw";
+        prawy.style.width = "47vw";
+        lewy.innerHTML = "";
+        prawy.innerHTML = "";
+        lewy.style.backgroundImage = "url('driveprawy.gif')";
+        lewy.style.backgroundSize = "cover";
+        lewy.style.backgroundPosition = "left";
+        lewy.style.float = "left";
+        prawy.style.backgroundImage = "url('drivelewy.gif')";
+        prawy.style.backgroundSize = "cover";
+        prawy.style.backgroundPosition = "right";
+        prawy.style.float = "left";
+        animateDivs();
+        
+        setTimeout(function(){
+          
+          const dialog = document.createElement("div");
+          dialog.classList.add("dialog");
+          dialog.innerHTML = "chuj ci w dupe luger";
+          dialog.setAttribute("onclick", "intro()");
+          
+          lewy.appendChild(dialog);
+          
+        }, 7000)
+    }, 1800);
+}
